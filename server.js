a67d69f3c586e92e303ca2d15fb3b7bdde4bee72
Maciek Stasieluk <macrusher@gmail.com>
2017-01-16 23:34:42 +0100

    Perform cleanup on Meteor server restart (closes #4)

--- server.js
+++ server.js
@@ -18,8 +18,25 @@ export function start () {
         // Start Selenium (this will log errors internally)
         const selenium = await spawnSelenium();
 
-        // Start and wait until the browser is ready
+        // Perform cleanup if Meteor server restarts while the tests are running
+        const cleanup = async () => {
+            // Close all browser windows
+            await browser.endAll().catch(() => {/* It could fail, it's ok :) */});
+
+            // Stop selenium server
+            selenium.kill();
+
+            console.info('[universe:e2e] Meteor server restart detected, interrupting ongoing tests...');
+
+            // Actually stop the Meteor process
+            process.exit(0);
+        };
+
+        // Since Meteor doesn't provide any exit hook, listening for SIGTERM is best what we can do
+        process.once('SIGTERM', cleanup);
+
         try {
+            // Start and wait until the browser is ready
             await browser.init();
         } catch (e) {
             // Re-throw to prevent further execution
@@ -29,27 +46,26 @@ export function start () {
 
         // Run the tests using Mocha
         mocha.run(async errorCount => {
-            // Close all browser sessions
+
+            // Gracefully stop WebDriverIO and Selenium
             try {
                 await browser.endAll();
             } catch (e) {
                 console.error(`[universe:e2e] Cannot close WebdriverIO sessions: ${e.message}`);
             }
 
-            // Stop Selenium
             try {
                 selenium.kill();
             } catch (e) {
                 console.error(`[universe:e2e] Cannot stop Selenium process: ${e.message}`);
             }
 
+            // We don't need to stop services on server restart anymore
+            process.removeListener('SIGTERM', cleanup);
+
             // In CI mode we stop Meteor, otherwise (watch mode) we leave it as is
             if (process.env.CI) {
-                if (errorCount > 0) {
-                    process.exit(1);
-                } else {
-                    process.exit(0);
-                }
+                process.exit(errorCount > 0 ? 1 : 0);
             }
         });
     });
